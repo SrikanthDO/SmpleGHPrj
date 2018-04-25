460ddd21f73e109d7d42d8c8c204e9b8b267035c
SrikanthDO <srikanth.akkapu@dennisone.in>
2018-04-25 18:17:10 +0530

    Initial Checkin for build/js/app.js

---
+++ build/js/app.js
@@ -0,0 +1,262 @@
+/**
+
+ *
+ * Custom scripts
+ */
+
+$(document).ready(function () {
+
+
+    // Full height of sidebar
+    function fix_height() {
+        var heightWithoutNavbar = $("body > #wrapper").height() - 61;
+        $(".sidebard-panel").css("min-height", heightWithoutNavbar + "px");
+
+        var navbarHeigh = $('nav.navbar-default').height();
+        var wrapperHeigh = $('#page-wrapper').height();
+
+        if(navbarHeigh > wrapperHeigh){
+            $('#page-wrapper').css("min-height", navbarHeigh + "px");
+        }
+
+        if(navbarHeigh < wrapperHeigh){
+            $('#page-wrapper').css("min-height", $(window).height()  + "px");
+        }
+
+    }
+
+    $(window).bind("load resize scroll", function() {
+        if(!$("body").hasClass('body-small')) {
+                fix_height();
+        }
+    })
+
+    // Move right sidebar top after scroll
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 0 && !$('body').hasClass('fixed-nav') ) {
+            $('#right-sidebar').addClass('sidebar-top');
+        } else {
+            $('#right-sidebar').removeClass('sidebar-top');
+        }
+    });
+
+    setTimeout(function(){
+        fix_height();
+    });
+
+});
+
+// Minimalize menu when screen is less than 768px
+$(function() {
+	$('body').addClass('skin-3');
+    $(window).bind("load resize", function() {
+        if ($(this).width() < 769) {
+            $('body').addClass('body-small')
+        } else {
+            $('body').removeClass('body-small')
+        }
+    })
+})
+
+/**
+ * SmpleGHPrj
+ */
+(function () {
+    angular.module('SmpleGHPrj', [
+        'ngResource', 
+        'oitozero.ngSweetAlert',
+        'angularFileUpload' ,
+        'ui.router',                   
+        'oc.lazyLoad',                
+        'ui.bootstrap',               
+         'cgNotify',
+         'datePicker',
+         'doneComponentsSet'
+    ])
+})();
+
+
+function getBaseURL() {
+	   return location.protocol + "//" + location.hostname + 
+	      (location.port && ":" + location.port) ;
+}; 
+function getAppName(p) {
+   return "/";
+}
+var _appUrl = getBaseURL()+getAppName(window.location.pathname);
+
+
+/**
+ * SmpleGHPrj 
+ * Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+    $urlRouterProvider.otherwise("/index/main");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
+
+    $stateProvider
+
+        .state('index', {
+            abstract: true,
+            url: "/index",
+            templateUrl: "common/content.html",
+        })
+        .state('index.main', {
+            url: "/main",
+            templateUrl: "main.html",
+            data: { pageTitle: 'Example view' }
+        })
+        .state('index.minor', {
+            url: "/minor",
+            templateUrl: "minor.html",
+            data: { pageTitle: 'Example view' }
+        })
+}
+angular
+    .module('SmpleGHPrj')
+    .config(config)
+    .run(function($rootScope, $state) {
+        $rootScope.$state = $state;
+        $rootScope.appName = "SmpleGHPrj";
+    });
+
+
+
+
+/**
+ * pageTitle - Directive for set Page title - mata title
+ */
+function pageTitle($rootScope, $timeout) {
+    return {
+        link: function(scope, element) {
+            var listener = function(event, toState, toParams, fromState, fromParams) {
+                // Default title - load on Dashboard 1
+                var title = 'SmpleGHPrj | Responsive Admin Theme';
+                // Create your own title pattern
+                if (toState.data && toState.data.pageTitle) title = 'SmpleGHPrj | ' + toState.data.pageTitle;
+                $timeout(function() {
+                    element.text(title);
+                });
+            };
+            $rootScope.$on('$stateChangeStart', listener);
+        }
+    }
+};
+
+/**
+ * sideNavigation - Directive for run metsiMenu on sidebar navigation
+ */
+function sideNavigation($timeout) {
+    return {
+        restrict: 'A',
+        link: function(scope, element) {
+            // Call the metsiMenu plugin and plug it to sidebar navigation
+            $timeout(function(){
+                element.metisMenu();
+            });
+        }
+    };
+};
+
+/**
+ * iboxTools - Directive for iBox tools elements in right corner of ibox
+ */
+function iboxTools($timeout) {
+    return {
+        restrict: 'A',
+        scope: true,
+        templateUrl: 'views/common/ibox_tools.html',
+        controller: function ($scope, $element) {
+            // Function for collapse ibox
+            $scope.showhide = function () {
+                var ibox = $element.closest('div.ibox');
+                var icon = $element.find('i:first');
+                var content = ibox.find('div.ibox-content');
+                content.slideToggle(200);
+                // Toggle icon from up to down
+                icon.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+                ibox.toggleClass('').toggleClass('border-bottom');
+                $timeout(function () {
+                    ibox.resize();
+                    ibox.find('[id^=map-]').resize();
+                }, 50);
+            },
+                // Function for close ibox
+                $scope.closebox = function () {
+                    var ibox = $element.closest('div.ibox');
+                    ibox.remove();
+                }
+        }
+    };
+};
+
+/**
+ * minimalizaSidebar - Directive for minimalize sidebar
+*/
+function minimalizaSidebar($timeout) {
+    return {
+        restrict: 'A',
+        template: '<a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="" ng-click="minimalize()"><i class="fa fa-bars"></i></a>',
+        controller: function ($scope, $element) {
+            $scope.minimalize = function () {
+                $("body").toggleClass("mini-navbar");
+                if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
+                    // Hide menu in order to smoothly turn on when maximize menu
+                    $('#side-menu').hide();
+                    // For smoothly turn on menu
+                    setTimeout(
+                        function () {
+                            $('#side-menu').fadeIn(500);
+                        }, 100);
+                } else if ($('body').hasClass('fixed-sidebar')){
+                    $('#side-menu').hide();
+                    setTimeout(
+                        function () {
+                            $('#side-menu').fadeIn(500);
+                        }, 300);
+                } else {
+                    // Remove all inline style from jquery fadeIn function to reset menu state
+                    $('#side-menu').removeAttr('style');
+                }
+            }
+        }
+    };
+};
+
+
+
+/**
+ *
+ * Pass all functions into module
+ */
+angular
+    .module('SmpleGHPrj')
+    .directive('pageTitle', pageTitle)
+    .directive('sideNavigation', sideNavigation)
+    .directive('iboxTools', iboxTools)
+    .directive('minimalizaSidebar', minimalizaSidebar)
+
+
+
+/**
+ * MainCtrl - controller
+ */
+function MainCtrl() {
+
+    this.userName = 'Example user';
+    this.helloText = 'Welcome in SeedProject';
+    this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
+
+};
+
+
+angular
+    .module('SmpleGHPrj')
+    .controller('MainCtrl', MainCtrl)
